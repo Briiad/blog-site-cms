dc8ee35cded46ef2588fce68a97037149a0e87d7
Brian Aditya <brianadit33@gmail.com>
2022-08-16 16:45:52 +0700

    catergories bar

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Link from 'next/link'
+
+import { getCategories } from '../services'
 
 export const Categories = () => {
+
+  const [categories, setCategories] = useState<any[]>([]);
+
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, [])
+
   return (
-    <div>Categories</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='lg:text-lg 2xl:text-xl mb-8 font-semibold border-b pb-4'>
+        Categories
+      </h3>
+      {categories.map((category) => (
+        <Link href={`/category/${category.slug}`} key={category.slug}>
+          <span className='cursor-pointer block pb-3 mb-3'>
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
   )
 }
 
