01a2f6f96f4066b3e9e99d88404db8cfad3744a7
Brian Aditya <brianadit33@gmail.com>
2022-08-16 16:45:09 +0700

    add new fetch

--- services/index.js
+++ services/index.js
@@ -35,3 +35,58 @@ export const getPosts = async () => {
   const result = await request(graphQLAPI, query)
   return result.postsConnection.edges
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+      orderBy: createdAt_ASC, 
+      last: 3
+      ) {
+        title
+        featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphQLAPI, query)
+  return result.posts
+}
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND:{categories_some: {slug_in: $categories}}}
+        last: 3
+      ){
+        title
+        featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  const result = await request(graphQLAPI, query)
+  return result.posts
+}
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+    `
+
+  const result = await request(graphQLAPI, query)
+  return result.categories
+}
