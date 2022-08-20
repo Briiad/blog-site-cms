22698866cf62f30940ad2e49755bb21d41e6492e
Brian Aditya <brianadit33@gmail.com>
2022-08-21 00:57:06 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home ({ posts }) {
 
         <div className="lg:col-span-4 col-span-1">
             <div className='lg:sticky relative top-8'>
-              <PostWidget />
+              <PostWidget categories={posts.categories} slug={posts.slug} />
               <Categories />  
             </div>
         </div>
