10db937f0907fb96694a6eebdb98436d8484f4cd
Brian Aditya <brianadit33@gmail.com>
2022-08-21 00:59:06 +0700

    change extension

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 
 // import modules and components
 import { Categories, PostWidget, PostCard } from '../components'
-import {getPosts} from '../services'
+import { getPosts } from '../services'
 
-export default function Home ({ posts }) {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,17 +17,17 @@ export default function Home ({ posts }) {
 
       <div className="grid grid-cols-1 lg:grid-cols-10 2xl:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-6 2xl:col-span-8">
-          {posts.map((post) => ( <PostCard key={post.title} post={post.node} /> ))}
+          {posts.map((post) => (<PostCard key={post.title} post={post.node} />))}
         </div>
 
         <div className="lg:col-span-4 col-span-1">
-            <div className='lg:sticky relative top-8'>
-              <PostWidget categories={posts.categories} slug={posts.slug} />
-              <Categories />  
-            </div>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
-        
+
     </div>
   )
 }
@@ -37,6 +36,6 @@ export async function getStaticProps() {
   const posts = (await getPosts()) || [];
 
   return {
-    props: {posts}
+    props: { posts }
   }
 }
